4caa3a6d300d4c9777235588771e7849b005216f
prwn90 <purwin.daniel@gmail.com>
2020-08-14 13:29:32 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,202 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import Row from "./Row";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var symbolsMap = {
+  2: ["marking", "32"],
+  0: ["marking marking-x", 9587],
+  1: ["marking marking-o", 9711]
+};
+
+//Orientations board
+var patterns = [
+  //Horizontal orientation
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  //Vertical oreintation
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  //Diagonal orientation
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+var AIScore = { 2: 1, 0: 2, 1: 0 };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardState: new Array(9).fill(2),
+      turn: 0,
+      active: true,
+      mode: "AI"
+    };
+    this.handleNewMove = this.handleNewMove.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleModeChange = this.handleModeChange.bind(this);
+    this.processBoard = this.processBoard.bind(this);
+    this.makeAIMove = this.makeAIMove.bind(this);
+  }
+  //Board game
+  processBoard() {
+    var won = false;
+    patterns.forEach(pattern => {
+      var firstMark = this.state.boardState[pattern[0]];
+
+      if (firstMark != 2) {
+        var marks = this.state.boardState.filter((mark, index) => {
+          return pattern.includes(index) && mark == firstMark;
+        });
+
+        if (marks.length == 3) {
+          document.querySelector("#message1").innerHTML =
+            String.fromCharCode(symbolsMap[marks[0]][1]) + " WIN!";
+          document.querySelector("#message1").style.display = "block";
+          pattern.forEach(index => {
+            var id = index + "-" + firstMark;
+            document.getElementById(id).parentNode.style.background = "#bfd9c5";
+          });
+          this.setState({ active: false });
+          won = true;
+        }
+      }
+    });
+
+    if (!this.state.boardState.includes(2) && !won) {
+      document.querySelector("#message2").innerHTML = "GAME OVER! DRAW!";
+      document.querySelector("#message2").style.display = "block";
+      this.setState({ active: false });
+    } else if (this.state.mode == "AI" && this.state.turn == 1 && !won) {
+      this.makeAIMove();
+    }
+  }
+  //Computers moves
+  makeAIMove() {
+    var emptys = [];
+    var scores = [];
+    this.state.boardState.forEach((mark, index) => {
+      if (mark == 2) emptys.push(index);
+    });
+
+    emptys.forEach(index => {
+      var score = 0;
+      patterns.forEach(pattern => {
+        if (pattern.includes(index)) {
+          var xCount = 0;
+          var oCount = 0;
+          pattern.forEach(p => {
+            if (this.state.boardState[p] == 0) xCount += 1;
+            else if (this.state.boardState[p] == 1) oCount += 1;
+            score += p == index ? 0 : AIScore[this.state.boardState[p]];
+          });
+          if (xCount >= 2) score += 10;
+          if (oCount >= 2) score += 20;
+        }
+      });
+      scores.push(score);
+    });
+
+    var maxIndex = 0;
+    scores.reduce(function(maxVal, currentVal, currentIndex) {
+      if (currentVal >= maxVal) {
+        maxIndex = currentIndex;
+        return currentVal;
+      }
+      return maxVal;
+    });
+    this.handleNewMove(emptys[maxIndex]);
+  }
+  //Reset Game
+  handleReset(e) {
+    if (e) e.preventDefault();
+    document
+      .querySelectorAll(".alert")
+      .forEach(el => (el.style.display = "none"));
+    this.setState({
+      boardState: new Array(9).fill(2),
+      turn: 0,
+      active: true
+    });
+  }
+  //New game move
+  handleNewMove(id) {
+    this.setState(
+      prevState => {
+        return {
+          boardState: prevState.boardState
+            .slice(0, id)
+            .concat(prevState.turn)
+            .concat(prevState.boardState.slice(id + 1)),
+          turn: (prevState.turn + 1) % 2
+        };
+      },
+      () => {
+        this.processBoard();
+      }
+    );
+  }
+  //Change mode game 
+  handleModeChange(e) {
+    e.preventDefault();
+    if (e.target.getAttribute("href").includes("AI")) {
+      e.target.style.background = "#d4edda";
+      document.querySelector("#twop").style.background = "none";
+      this.setState({ mode: "AI" });
+      this.handleReset(null);
+    } else if (e.target.getAttribute("href").includes("2P")) {
+      e.target.style.background = "#d4edda";
+      document.querySelector("#ai").style.background = "none";
+      this.setState({ mode: "2P" });
+      this.handleReset(null);
+    }
+  }
+
+  render() {
+    const rows = [];
+    for (var i = 0; i < 3; i++)
+      rows.push(
+        <Row
+          row={i}
+          boardState={this.state.boardState}
+          onNewMove={this.handleNewMove}
+          active={this.state.active}
+        />
+      );
+    return (
+      <div>
+        <div class="container jumbotron" id="container">
+          <h3>Tic-Tac-Toe Game! ❌⭕</h3>
+          <p>
+            🔹
+            <a href="./?2P" onClick={this.handleModeChange} id="twop">
+              {" "}
+              2 PLAYERS MODE
+            </a>{" "}
+            🔹
+            <a href="./?AI" onClick={this.handleModeChange} id="ai">
+              PLAY WITH COMPUTER
+            </a>{" "}
+            🔹
+            <a href="#" onClick={this.handleReset}>
+              {" "}
+              RESET GAME
+            </a>
+            🔹
+          </p>
+          <p>{String.fromCharCode(symbolsMap[this.state.turn][1])} TURN</p>
+          <div className="board">{rows}</div>
+          <p class="alert alert-success" role="alert" id="message1"></p>
+          <p class="alert alert-info" role="alert" id="message2"></p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
